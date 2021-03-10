d98b5dfd331eecf3d79a6d9cea689ac89f6c9d09
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-10 22:02:08 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace HomeWork5_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        
+            var InputText = Console.ReadLine();
+            File.WriteAllLines("Homework5.1.txt", new[] { InputText });
+
+
+        }
+    } 
+}
